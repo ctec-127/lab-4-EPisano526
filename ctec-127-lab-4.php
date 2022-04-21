dc8d7667eb8ac6712d6de5f5cdaf83439543ff7c
Esther Pisano <E.PISANO@students.clark.edu>
2022-04-21 10:57:47 -0700

    branch1

--- ctec-127-lab-4.php
+++ ctec-127-lab-4.php
@@ -28,3 +28,21 @@ require "inc/contact.html";
 
 // include footer.html
 require "inc/footer.html";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 4</title>
+</head>
+
+<body>
+    <h3>Update by Esther 2022</h3>
+</body>
+
+</html>
